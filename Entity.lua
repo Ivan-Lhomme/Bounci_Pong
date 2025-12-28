0a129e72f519e2bd280674f5377a428612c3b8e0
Ivan <i.lhommeageorges@gmail.com>
2025-12-28 15:41:20 +0100

    Add angle reset when entity is reset to prevent bug

--- Entity.lua
+++ Entity.lua
@@ -18,6 +18,7 @@ end
 function Entity:reset()
     self.collider:setPosition(self.x + 5, self.startY + 100)
     self.collider:setLinearVelocity(0, 0)
+    self.collider:setAngle(0)
 end
 
 function Entity:draw()
