60d8b8ec92c6921cbb49fb198f50938f0cd0285a
Ivan <i.lhommeageorges@gmail.com>
2025-12-28 11:56:35 +0100

    Rename strartVelocity to velocity and reset it in reset() method

--- Ball.lua
+++ Ball.lua
@@ -1,14 +1,14 @@
 Ball = {}
 Ball.__index = Ball
 
+ball.velocity = {-200, 200}
+
 function Ball:new(world)
     local ball = {}
     setmetatable(ball, self)
 
     ball.startX = 395
     ball.startY = 295
-
-    ball.startVelocity = {-200, 200}
 
     ball.collider = world:newRectangleCollider(ball.startX, ball.startY, 10, 10)
     ball.collider:setType('kinematic')
@@ -18,6 +18,7 @@ function Ball:new(world)
 end
 
 function Ball:reset()
+    self.velocity = {-200, 200}
     self.collider:setPosition(self.startX, self.startY)
     self.collider:setLinearVelocity(self.startVelocity[love.math.random(1, 2)], 0)
 end
