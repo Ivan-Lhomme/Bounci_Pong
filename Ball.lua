b79adf48240088049c72b0ec97a4f0eab194edbe
Ivan <i.lhommeageorges@gmail.com>
2026-01-06 11:44:19 +0100

    Optimise function touch from ball

--- Ball.lua
+++ Ball.lua
@@ -32,6 +32,31 @@ function Ball:draw()
     colliderDraw(self.collider)
 end
 
+function Ball:wallCollision(y1, vyChoose)
+    local x1 = 400
+    local x2, y2 = self.collider:getPosition()
+
+    local vx, vy = self.collider:getLinearVelocity()
+
+    local distance = math.sqrt((x2 - x1)^2 + (y2 - y1)^2)*1.5
+    self.vy[1] = -distance
+    self.vy[2] = distance
+    
+    if (self.vx[2] + 40) <= 640 then
+        self.vx[1] = self.vx[1] - 40
+        self.vx[2] = self.vx[2] + 40
+    elseif not (self.vx[2] == 640) then
+        self.vx[2] = 640
+    end
+
+    local vxChoose = 2
+    if vx < 0 then
+        vxChoose = 1
+    end
+
+    self.collider:setLinearVelocity(self.vx[vxChoose], self.vy[vyChoose])
+end
+
 function Ball:touch()
     local x2, y2 = self.collider:getPosition()
 
@@ -64,53 +89,11 @@ function Ball:touch()
     end
 
     if y2 < 0  then
-        local x1, y1 = 400, -1
-        local x2, y2 = self.collider:getPosition()
-
-        local vx, vy = self.collider:getLinearVelocity()
-
-        local distance = math.sqrt((x2 - x1)^2 + (y2 - y1)^2)*1.5
-        self.vy[1] = -distance
-        self.vy[2] = distance
-        
-        if (self.vx[2] + 40) <= 640 then
-            self.vx[1] = self.vx[1] - 40
-            self.vx[2] = self.vx[2] + 40
-        elseif not (self.vx[2] == 640) then
-            self.vx[2] = 640
-        end
-
-        local vChoose = {2, 2}
-        if vx < 0 then
-            vChoose[1] = 1
-        end
-
-        self.collider:setLinearVelocity(self.vx[vChoose[1]], self.vy[vChoose[2]])
+        self:wallCollision(-1, 2)
     end
 
     if y2 > 600  then
-        local x1, y1 = 400, 601
-        local x2, y2 = self.collider:getPosition()
-
-        local vx, vy = self.collider:getLinearVelocity()
-
-        local distance = math.sqrt((x2 - x1)^2 + (y2 - y1)^2)*1.5
-        self.vy[1] = -distance
-        self.vy[2] = distance
-        
-        if (self.vx[2] + 40) <= 640 then
-            self.vx[1] = self.vx[1] - 40
-            self.vx[2] = self.vx[2] + 40
-        elseif not (self.vx[2] == 640) then
-            self.vx[2] = 640
-        end
-
-        local vChoose = {2, 1}
-        if vx < 0 then
-            vChoose[1] = 1
-        end
-
-        self.collider:setLinearVelocity(self.vx[vChoose[1]], self.vy[vChoose[2]])
+        self:wallCollision(801, 1)
     end
 
     if x2 < -2 or x2 > 802 then
